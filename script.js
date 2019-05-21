ab79773932fbe526f050a16de265cb0a6bce246d
Matt Garcia <mattrg1105@gmail.com>
2019-05-20 19:27:04 -0700

    added render for names and details

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const fetchData = async() => {
   try {
-    const response = await fetch('https://swapi.co/api/people/');
+    const response = await fetch('https://swapi.co/api/people/?page=1');
     const data = await response.json();
     const names = data.results.map( item => item.name);
 
     const html = names.map( name => `<li>${name}</li>`).join('');
-    const list = document.querySelector('ul');
+    const list = document.querySelector('.names');
     list.innerHTML = html;
 
+    const listItem = document.querySelector('ul');
 
+    listItem.addEventListener('click', (event) => {
+    const htmlName = event.target.innerHTML;
+    let details = '';
+    let arr = [];
+    for (let i = 0; i < data.results.length; i++){
+      if (data.results[i].name === htmlName){
+        details = data.results[i];
+      }
+     Object.entries(details).forEach(([key, value]) => arr.push(`${key}: ${value}`));
+      // for (let key in details){
+      //   if(){
+      //   arr.push(key+':'+details[key]);
+      //   }
+      // }
+      //console.log(arr);
+
+     const detailHTML = arr.map( item => `<li>${item}</li>`).join('');
+     const detailClass = document.querySelector('.details');
+     detailClass.innerHTML = detailHTML;
+
+    }
+
+  })
   }
   catch (err){
     console.log(err);
